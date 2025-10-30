5e7cef27f5949140ab06ac08dc5dd1c6d633b5d0
pvashisth-07 <pvashisth0711@gmail.com>
2025-10-30 10:51:52 +0530

    updated nodes

--- src/Tools/tool_assembly.py
+++ src/Tools/tool_assembly.py
@@ -2,16 +2,17 @@ from langgraph.prebuilt import ToolNode
 from src.Tools.search_tool import get_search_tool
 from src.Tools.hotel_tool import fetch_hotel_rates
 
-def get_tools():
-    """Returns the list of tools used in the chatbot"""
+class Tools:
+    def get_tools():
+        """Returns the list of tools used in the chatbot"""
 
-    tools=[get_search_tool,fetch_hotel_rates]
+        tools=[get_search_tool,fetch_hotel_rates]
 
-    return tools
+        return tools
 
-def create_tool_node(tools):
-    """
-    creates and return a tool node for graph.
-    """
+    def create_tool_node(tools):
+        """
+        creates and return a tool node for graph.
+        """
 
-    return ToolNode(tools=tools)
+        return ToolNode(tools=tools)
--- src/nodes/itenary_node.py
+++ src/nodes/itenary_node.py
@@ -1,6 +1,6 @@
 from src.fine_tuning.llm_tuning import Groqllm
 from src.State.state import State
-
+from src.Tools.tool_assembly import Tools
 class ItenaryNode:
     """
     Itenary Node implementation
@@ -13,5 +13,7 @@ class ItenaryNode:
         """
         Process the input state and generate an itenary response.
         """
+        final_query=state['structured_query']
+        
 
         return {"itenary":self.llm.invoke(state["query"])}
